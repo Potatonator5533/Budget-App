6e321e1982655f653319352798802f932c91abaa
Potatonator5533 <josue04rosales@gmail.com>
2024-02-15 15:48:11 -0800

    first commit

---
+++ script/app.js
@@ -0,0 +1,7 @@
+import { saveToLocalStorage, getLocalStorage, removeFromLocalStorage } from "./localStorage.js";
+
+let budget = document.getElementById("budget");
+let updateBudget = document.getElementById("updateBudget");
+let addExpenses = document.getElementById("addExpenses");
+let manageExpenses = document.getElementById("manageExpenses");
+
